f6cf5ca50f9ebd0f986a419c20953bce777c6f20
kimkyunghun3 <kimkyunghun3@naver.com>
2019-09-29 22:04:16 +0900

    save local about location, latitude,longitude

--- weather.js
+++ weather.js
@@ -1,7 +1,18 @@
 const COORDS = 'coords';
 
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+
+}
+
 function handleGeoSucces(position){
-    console.log(position);
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj={
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);
 }
 function handleGeoError(){
     console.log('cant access geo location');
