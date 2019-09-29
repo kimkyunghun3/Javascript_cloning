b6e0e00c20d4584b3cd5bce4155137c1cece4cac
kimkyunghun3 <kimkyunghun3@naver.com>
2019-09-29 22:21:07 +0900

    get weather, can check on conssole

--- weather.js
+++ weather.js
@@ -1,4 +1,9 @@
+const API_KEY ="19eaa6618738120726476ee57c29f301";
 const COORDS = 'coords';
+
+function getweather(lat,log){
+    fetch(`api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`)
+}; 
 
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -13,21 +18,24 @@ function handleGeoSucces(position){
         longitude
     };
     saveCoords(coordsObj);
+    getweather(latitude,longitude);
 }
 function handleGeoError(){
     console.log('cant access geo location');
-}
+} 
 
 
 function askForCoords(){
     navigator.geolocation.getCurrentPosition(handleGeoSucces,handleGeoError)
 }
 function loadCoords(){
-    const loadedCords =localStorage.getItem(COORDS);
-    if(loadedCords ==null){
+    const loadedCoords =localStorage.getItem(COORDS);
+    if(loadedCoords ==null){
         askForCoords();
 
     }else{
+      const parseCoords =JSON.parse(loadedCoords);
+      console.log(parseCoords);
         //getweather
     }
 }
